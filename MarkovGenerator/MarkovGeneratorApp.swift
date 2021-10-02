25f2dabf9152c5176cee13ee4a78faac336caebb
Nicholas Verrico <nicholas.verrico@icloud.com>
2021-10-02 15:33:58 -0400

    Initial Commit

---
+++ MarkovGenerator/MarkovGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkovGeneratorApp.swift
+//  MarkovGenerator
+//
+//  Created by Nicholas Verrico on 10/2/21.
+//
+
+import SwiftUI
+
+@main
+struct MarkovGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
